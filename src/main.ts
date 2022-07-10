77009846f14243975429872acb669275906fbef1
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-07-10 18:38:04 -0300

    chore: install PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'primevue/resources/themes/saga-blue/theme.css';
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue)
+app.component('Button', Button)
+app.mount('#app')
+
+
