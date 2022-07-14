4a13e1cf24fb9ea4f92945ec8ae04c878f8da2ac
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-07-14 18:50:42 -0300

    feat: uses i18n to show texts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'primevue/resources/themes/saga-blue/theme.css';
-import PrimeVue from 'primevue/config';
-import Button from 'primevue/button';
+import router from './router'
+import 'primeicons/primeicons.css';
+import { createI18n } from 'vue-i18n'
+import messages from './locales/messages.json'
+
+const i18n = createI18n({
+  locale: window.localStorage.getItem('lang') || 'en',
+  fallbackLocale: 'pt-br',
+  messages
+})
 
 const app = createApp(App)
-app.use(PrimeVue)
-app.component('Button', Button)
+app.use(router)
+app.use(i18n)
 app.mount('#app')
 
 
